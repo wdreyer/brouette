f83f9b6e8374521ba9a69b0efa0d16974460adae
Your name <dreyer.wil@gmail.com>
2026-02-13 02:16:03 +0100

    test deploy

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
